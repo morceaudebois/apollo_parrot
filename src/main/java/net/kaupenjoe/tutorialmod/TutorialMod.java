6c91c8576784a789a87edfd19f9aedf2506b4b34
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2023-09-16 15:23:19 +0200

    Added item

--- src/main/java/net/kaupenjoe/tutorialmod/TutorialMod.java
+++ src/main/java/net/kaupenjoe/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.kaupenjoe.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.kaupenjoe.tutorialmod.item.ModItems;
+import net.kaupenjoe.tutorialmod.util.ModLootTableModifiers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+
+		ModItems.registerModItems();
 	}
 }
