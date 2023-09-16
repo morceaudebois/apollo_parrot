022fa978cabd79a84ec1275a2e0ca63684380749
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2023-09-16 15:23:21 +0200

    Create ModLootTableModifiers.java

---
+++ src/main/java/net/kaupenjoe/tutorialmod/util/ModLootTableModifiers.java
@@ -0,0 +1,28 @@
+package net.kaupenjoe.tutorialmod.util;
+
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.kaupenjoe.tutorialmod.item.ModItems;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.util.Identifier;
+
+public class ModLootTableModifiers {
+    private static final Identifier DESERT_PYRAMID_ID =
+            new Identifier("minecraft", "archaeology:desert_pyramid");
+    public static void modifyLootTables() {
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if (DESERT_PYRAMID_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(1f)) // Drops 100% of the time
+                        .with(ItemEntry.builder(ModItems.THYLACINE_SKULL))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                tableBuilder.pool(poolBuilder.build());
+            }
+        });
+    }
+}
