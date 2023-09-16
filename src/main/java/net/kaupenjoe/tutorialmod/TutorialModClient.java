15687d23daae12239e8d4d15db99624e46e42d1f
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2023-09-16 14:09:58 +0200

    Choses

---
+++ src/main/java/net/kaupenjoe/tutorialmod/TutorialModClient.java
@@ -0,0 +1,13 @@
+package net.kaupenjoe.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'onInitializeClient'");
+    }
+    
+}
