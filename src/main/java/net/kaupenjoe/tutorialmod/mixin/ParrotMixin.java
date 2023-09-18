e8f12d9bf61bdaa2e3a61f084e9b0c4b47ca8d8f
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2023-09-18 12:09:26 +0200

    Registered the sounds

--- src/main/java/net/kaupenjoe/tutorialmod/mixin/ParrotMixin.java
+++ src/main/java/net/kaupenjoe/tutorialmod/mixin/ParrotMixin.java
@@ -16,20 +16,56 @@ public class ParrotMixin {
     @Inject(method = "getRandomSound", at = @At("RETURN"), cancellable = true)
     private static void injected(CallbackInfoReturnable<SoundEvent> cir) {
         if (cir.getReturnValue() == SoundEvents.ENTITY_PARROT_AMBIENT) {
-            SoundEvent[] soundEvents = {
+            SoundEvent[] ApolloSounds = {
+                    ModSounds.BOTTLE,
+                    ModSounds.APOLLO,
+                    ModSounds.BOOM,
+                    ModSounds.BOOP,
+                    ModSounds.CHICKEN,
+                    ModSounds.CORK,
                     ModSounds.GLASK,
-                    ModSounds.WARIO,
+                    ModSounds.GLASS_2,
+                    ModSounds.GLASS,
+                    ModSounds.GOOD_BOY,
+                    ModSounds.HELLO,
+                    ModSounds.LETSGO,
+                    ModSounds.METAL,
                     ModSounds.NO_CHEWING,
+                    ModSounds.NOISE1,
+                    ModSounds.PAPER,
+                    ModSounds.PEEKABOO,
+                    ModSounds.PLANT,
+                    ModSounds.ROCK,
+                    ModSounds.SAWTHEBIRD,
+                    ModSounds.SHREK,
+                    ModSounds.SHROCK,
+                    ModSounds.SLURP,
+                    ModSounds.SMALL_SUCK,
+                    ModSounds.SQUIRREL,
+                    ModSounds.STEP_UP,
+                    ModSounds.STOP_DOING_THE_CORN,
+                    ModSounds.SUCK1,
+                    ModSounds.SUCK2,
+                    ModSounds.THISISABOWL,
+                    ModSounds.TOUCHYOURBEAK,
+                    ModSounds.WANNASNACK,
+                    ModSounds.WARIO,
+                    ModSounds.WHAT_COLOR,
+                    ModSounds.WHATS_THIS,
+                    ModSounds.WHATS_THIS2,
+                    ModSounds.WOW
             };
+
 
             // Create a Random object
             Random random = new Random();
 
-            // Generate a random number from 0 to 2 (inclusive) to select one of three values
-            int randomNumber = random.nextInt(3);
+            if (random.nextInt(3) < 1) {
+                // Generate a random number from 0 to 2 (inclusive) to select one of three values
+                int randomNumber = random.nextInt(37);
+                cir.setReturnValue(ApolloSounds[randomNumber]);
+            }
 
-
-            cir.setReturnValue(soundEvents[randomNumber]);
         }
     }
 }
--- src/main/java/net/kaupenjoe/tutorialmod/sound/ModSounds.java
+++ src/main/java/net/kaupenjoe/tutorialmod/sound/ModSounds.java
@@ -10,9 +10,44 @@ import net.minecraft.util.Identifier;
 public class ModSounds {
     // ITEM_METAL_WHISTLE is the name of the custom sound event
     // and is called in the mod to use the custom sound
+    public static SoundEvent BOTTLE = registerSound("bottle");
+    public static SoundEvent APOLLO = registerSound("apollo");
+    public static SoundEvent BOOM = registerSound("boom");
+    public static SoundEvent BOOP = registerSound("boop");
+    public static SoundEvent CHICKEN = registerSound("chicken");
+    public static SoundEvent CORK = registerSound("cork");
     public static SoundEvent GLASK = registerSound("glask");
+    public static SoundEvent GLASS_2 = registerSound("glass_2");
+    public static SoundEvent GLASS = registerSound("glass");
+    public static SoundEvent GOOD_BOY = registerSound("good_boy");
+    public static SoundEvent HELLO = registerSound("hello");
+    public static SoundEvent LETSGO = registerSound("letsgo");
+    public static SoundEvent METAL = registerSound("metal");
     public static SoundEvent NO_CHEWING = registerSound("no_chewing");
+    public static SoundEvent NOISE1 = registerSound("noise1");
+    public static SoundEvent PAPER = registerSound("paper");
+    public static SoundEvent PEEKABOO = registerSound("peekaboo");
+    public static SoundEvent PLANT = registerSound("plant");
+    public static SoundEvent ROCK = registerSound("rock");
+    public static SoundEvent SAWTHEBIRD = registerSound("sawthebird");
+    public static SoundEvent SHREK = registerSound("shrek");
+    public static SoundEvent SHROCK = registerSound("shrock");
+    public static SoundEvent SLURP = registerSound("slurp");
+    public static SoundEvent SMALL_SUCK = registerSound("small_suck");
+    public static SoundEvent SQUIRREL = registerSound("squirrel");
+    public static SoundEvent STEP_UP = registerSound("step_up");
+    public static SoundEvent STOP_DOING_THE_CORN = registerSound("stop_doing_the_corn");
+    public static SoundEvent SUCK1 = registerSound("suck1");
+    public static SoundEvent SUCK2 = registerSound("suck2");
+    public static SoundEvent THISISABOWL = registerSound("thisisabowl");
+    public static SoundEvent TOUCHYOURBEAK = registerSound("touchyourbeak");
+    public static SoundEvent WANNASNACK = registerSound("wannasnack");
     public static SoundEvent WARIO = registerSound("wario");
+    public static SoundEvent WHAT_COLOR = registerSound("what_color");
+    public static SoundEvent WHATS_THIS = registerSound("whats_this");
+    public static SoundEvent WHATS_THIS2 = registerSound("whats_this2");
+    public static SoundEvent WOW = registerSound("wow");
+
 
     // actual registration of all the custom SoundEvents
     static SoundEvent registerSound(String id) {
