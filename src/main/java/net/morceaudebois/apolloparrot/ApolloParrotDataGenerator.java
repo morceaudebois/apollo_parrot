4101cc8bb7eb6226b705a676b2cf5a7daac8bb15
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2023-10-05 19:31:40 +0200

    Not working datagen

--- src/main/java/net/morceaudebois/apolloparrot/ApolloParrotDataGenerator.java
+++ src/main/java/net/morceaudebois/apolloparrot/ApolloParrotDataGenerator.java
@@ -2,10 +2,17 @@ package net.morceaudebois.apolloparrot;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.morceaudebois.apolloparrot.datagen.*;
 
 public class ApolloParrotDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
---
+++ src/main/java/net/morceaudebois/apolloparrot/datagen/ModLootTableProvider.java
@@ -0,0 +1,15 @@
+package net.morceaudebois.apolloparrot.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+
+public class ModLootTableProvider extends FabricBlockLootTableProvider {
+    public ModLootTableProvider(FabricDataOutput dataOutput) {
+        super(dataOutput);
+    }
+
+    @Override
+    public void generate() {
+
+    }
+}
